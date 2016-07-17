06c27fc608dd4a38ee15833f49ca24bab26a69fd
Joshua Small <joshuahugh94@gmail.com>
2016-07-17 18:23:10 +0100

    Stylised the list to show the bikes in a grid

--- app/bikes/bikes.component.js
+++ app/bikes/bikes.component.js
@@ -14,7 +14,27 @@ angular.
 
         // Load in the bikes
         $http.get('//raw.githubusercontent.com/jujhars13/jujhars13/master/bikes.json').then(function(response) {
-            self.bikes = response.data.items;
+            var data = response.data.items;
+
+            self.bikeRows = [];
+
+            var bikeRowIndex = 0;
+            var cols = 4;
+
+            // Group the bikes into rows
+            for (var i = 0; i < data.length; i++) {
+                if (i % cols == 0 && i != 0)
+                    bikeRowIndex++;
+
+                // Create the array to hold the bikes in this row
+                if (typeof self.bikeRows[bikeRowIndex] === 'undefined')
+                    self.bikeRows[bikeRowIndex] = [];
+
+                // Urls are currently serivng a 403 so use this for now
+                data[i].image.thumb = 'bikes/bike.png';
+
+                self.bikeRows[bikeRowIndex].push(data[i]);
+            }
         });
     }]
     });
